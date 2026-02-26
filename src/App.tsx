0fb3f8aadf256418c6fc1619a90ead923c3a8dda
eman467 <aziz.eman@hotmail.com>
2026-02-25 21:46:24 -0500

    setup queryClient global config so that if the backend returns a 401 on any mutation request (put,post,delete), the react query cache for the logged in user is invalidated by setting it to null. Also created a useQuery fetch on app render to check if session exists and then fetching it to store in cache. setup routes for new pages login and profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,67 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import RootLayout from "./layouts/RootLayout/RootLayout";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 import HomeFeed from "./pages/HomeFeed";
 import PostDetails from "./pages/PostDetails";
+import Login from "./pages/Login";
+import userService from "./api/userService";
+import Profile from "./pages/Profile";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error: any) => {
+      if (error?.status === 401) {
+        //add some error conditions for api calls
+      }
+    },
+  }),
+  mutationCache: new MutationCache({
+    onError: (error: any) => {
+      if (error?.status === 401) {
+        queryClient.setQueryData(["me"], null);
+        window.location.href = "/login";
+      }
+    },
+  }),
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<RootLayout />}>
-            <Route index element={<HomeFeed />} />
-            <Route path="post/:communityId/:postId" element={<PostDetails />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <AppContent />
     </QueryClientProvider>
+  );
+}
+
+function AppContent() {
+  const { data: user } = useQuery({
+    queryKey: ["me"],
+    queryFn: userService.fetchMe,
+  });
+
+  console.log("fetched user: ", user);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<HomeFeed />} />
+          <Route path="post/:communityId/:postId" element={<PostDetails />} />
+          <Route path="profile/:profileId" element={<Profile />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
